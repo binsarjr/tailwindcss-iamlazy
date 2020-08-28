42d478b33cb32e4a31e7ab003cd934692a05f2cb
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2020-08-28 17:41:07 +0700

    editing badge component, new style

--- src/iamlazy/components/badge.js
+++ src/iamlazy/components/badge.js
@@ -12,12 +12,13 @@ module.exports = function({
 }) {
     const badge = {
         '.badge': {
-            padding: '.25em .4em',
-            fontSize: '75%',
-            fontWeight: theme('fontWeight.bold'),
+            paddingTop: theme('padding.1'),
+            paddingRight: theme('padding.2'),
+            paddingBottom: theme('padding.1'),
+            paddingLeft: theme('padding.2'),
+            fontSize: theme('fontSize.xs'),
             borderRadius: theme('borderRadius.default'),
             textAlign: 'center',
-            lineHeight: '1',
             whiteSpace: 'nowrap',
             verticalAlign: 'baseline',
             display: 'inline-block',
@@ -29,43 +30,65 @@ module.exports = function({
 
     const badgeColors = {
         // key: className
-        'gray': '.badge-gray,.badge-secondary',
-        'red': '.badge-red,.badge-danger',
-        'orange': '.badge-orange',
-        'yellow': '.badge-yellow,.badge-warning',
-        'green': '.badge-green,.badge-success',
-        'teal': '.badge-teal,.badge-info',
         'blue': '.badge-blue,.badge-primary',
+        'red': '.badge-red,.badge-danger',
         'indigo': '.badge-indigo',
         'purple': '.badge-purple',
         'pink': '.badge-pink'
     }
 
+
+    for (badgeColor in badgeColors) {
+        badge[badgeColors[badgeColor]] = {
+            backgroundColor: theme(`backgroundColor.${badgeColor}.500`),
+            color: theme('colors.gray.200')
+        }
+    }
+
+    // secondary
+    badge['.badge-secondary,.badge-gray'] = {
+        backgroundColor: theme('colors.gray.600'),
+        color: theme('colors.gray.100')
+    }
+
+    // info
+    badge['.badge-info,.badge-teal'] = {
+        backgroundColor: theme('colors.teal.400'),
+        color: theme('colors.gray.100')
+    }
+
+
+    // success
+    badge['.badge-success,.badge-green'] = {
+        backgroundColor: theme('colors.green.400'),
+        color: theme('colors.gray.100')
+    }
+
+    // warning
+    badge['.badge-warning,.badge-yellow'] = {
+        backgroundColor: theme('colors.yellow.400'),
+        color: theme('colors.gray.800')
+    }
+
+
+    // orange
+    badge['.badge-orange'] = {
+        backgroundColor: theme('colors.orange.400'),
+        color: theme('colors.gray.800')
+    }
+
     // light
     badge['.badge-light'] = {
-        backgroundColor: lightColor,
-        color: theme('colors.gray.700')
-    }
-    badge['a.badge-light:hover:not(.disabled)'] = {
-        backgroundColor: theme('backgroundColor.gray.200'),
+        backgroundColor: theme('colors.gray.100'),
+        color: theme('colors.gray.900')
     }
 
     // dark
     badge['.badge-dark'] = {
-        backgroundColor: theme(`backgroundColor.gray.700`),
-        color: theme('colors.white')
+        backgroundColor: theme(`colors.gray.900`),
+        color: theme('colors.gray.100')
     }
-    badge['a.badge-dark:hover:not(.disabled)'] = {
-        backgroundColor: theme(`backgroundColor.black`),
-    }
-    for (badgeColor in badgeColors) {
-        badge[badgeColors[badgeColor]] = {
-            backgroundColor: theme(`backgroundColor.${badgeColor}.500`),
-            color: theme('colors.white')
-        }
-        badge[`a${badgeColors[badgeColor].split(',').join(',a')}:hover:not(.disabled)`] = {
-            backgroundColor: theme(`backgroundColor.${badgeColor}.600`),
-        }
-    }
+
+
     addComponents([badge])
 }
